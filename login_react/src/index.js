748100f2e46e0eff9a906a46718a9ed415e8d864
victor77dev <victor77.dev@gmail.com>
2018-05-05 14:19:24 +0200

    Basic layouts and pages for login system

--- login_react/src/index.js
+++ login_react/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Layout from './components/layout/Layout';
+
+import Home from './page/Home';
+import Register from './page/Register';
+import Login from './page/Login';
+import Logout from './page/Logout';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path='/' component={Layout} />
+      <Route exact path='/' component={Home} />
+      <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
+      <Route path='/logout' component={Logout} />
+    </div>
+  </Router>
+  , document.getElementById('root')
+);
---
+++ login_react/src/page/Logout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Page extends React.Component {
+  render() {
+    return (
+      <div>Welcome!!!</div>
+    );
+  }
+}
